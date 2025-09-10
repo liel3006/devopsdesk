cee583815f44dd902299845c6fa9423472c955df
Your Name <you@example.com>
2025-09-10 10:11:19 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask
+
+app = Flask(__name__)
+@app.get("/")
+def home():
+return ("בוקר טוב")
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5500)
---
+++ test.py
@@ -0,0 +1,40 @@
+# age=input("מה הגיל שלך?")
+
+# print(f"age:{int(age)+1}")
+
+# food=input("מה האוכל המועדף עלייך?")
+
+# name=input("מה השם שלך?")
+
+# print(f"your name is:{name} and your favorite food is:{food}")
+
+# numberone=input("מספר אחד")
+# numbertwo=input("מספר נוסף")
+# print(f"{int(numberone)+int(numbertwo)}")
+
+
+# print("liel")
+# print(7
+
+# print(3-1)
+# print(3*1)
+# print(3+1)
+# print(3/3)
+# print(3//3)
+# print(3**3)
+# print(6%10)
+# print(5%11)
+
+
+# s = float(input("מרחק"))
+# t = float(input("זמן"))
+#
+#
+# v=s/t
+#
+# print(f"{round(v,2)}")
+
+
+
+
+
