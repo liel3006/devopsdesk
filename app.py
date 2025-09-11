5132c8e74a79040a7ba2221752c510ae0d430ebb
Your Name <you@example.com>
2025-09-11 11:09:46 +0300

    add liel good morning in app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.get("/")
 def home():
-return ("בוקר טוב ליאל")
+    return ("בוקר טוב ליאל")
 # liel
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5500)
